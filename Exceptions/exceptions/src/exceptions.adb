a7b5b9184943515434d20f65927966a2cc63c9b2
Palmis <melvin_02@hotmail.se>
2022-11-01 09:41:29 +0100

    duggap2inskickad

--- Exceptions/exceptions/src/exceptions.adb
+++ Exceptions/exceptions/src/exceptions.adb
@@ -5,10 +5,8 @@ with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Exceptions;  use Ada.Exceptions;
 
 procedure Test_Exceptions is
-   --Exceptions del 2
-   --Length_Error : exception;
-
-   --Exceptions del 3
+   --Exceptions
+   Length_Error : exception;
    Format_Error : exception;
    Year_Error : exception;
    Month_Error : exception;
@@ -30,34 +28,64 @@ procedure Test_Exceptions is
 
    --PROCEDURES AND FUNCTIONS
    --DEL 1
-   --procedure Get_Value_Safe (Min, Max : in Integer;
-   --                        Value : out Integer) is
-   --begin
-   --   loop
-   --      Put("Mata in värde (");
-   --      Put(Min, Width => 0);
-   --      Put(" - ");
-   --      Put(Max, Width => 0);
-   --      Put("): ");
-   --      begin
-   --         Get(Value);
-   --         if Value < Min then
-   --            Put("För litet värde. ");
-   --         elsif Value > Max then
-   --            Put("För stort värde. ");
-   --         else
-   --            exit;
-   --         end if;
-   --   exception
-   --      when Data_Error => 
-   --         Put("Fel datatyp. ");
-   --         Skip_Line;
-   --      end;
-   --   end loop;
-   --end Get_Value_Safe;
+   procedure Get_Value_Safe (Min, Max : in Integer;
+                           Value : out Integer) is
+   begin
+      loop
+         Put("Mata in värde (");
+         Put(Min, Width => 0);
+         Put(" - ");
+         Put(Max, Width => 0);
+         Put("): ");
+         begin
+            Get(Value);
+            if Value < Min then
+               Put("För litet värde. ");
+            elsif Value > Max then
+               Put("För stort värde. ");
+            else
+               exit;
+            end if;
+      exception
+         when Data_Error => 
+            Put("Fel datatyp. ");
+            Skip_Line;
+         end;
+      end loop;
+   end Get_Value_Safe;
 
+   procedure Del1 (Min, Max, Value : out Integer) is
+   begin
+      Put_Line("Del 1:");
+      Put("Mata in Min och Max: ");
+      Get(Min);
+      Get(Max);
+      Get_Value_Safe(Min, Max, Value);
+      Put("Du matade in heltalet ");
+      Put(Value, Width => 0);
+      Put('.');
+      New_Line(2);
+      Skip_Line;
+   end Del1;
+
+   procedure Del2 (S_1,S_2,S_3 : out String) is
+   begin
+      Put_Line("Del 2:");
+      Put("Mata in en sträng på exakt 5 tecken: ");
+      Get(S_1);
+      Put_Line("Du matade in " & S_1);
+      Put("Mata in en sträng på exakt 6 tecken: ");
+      Get(S_2);
+      Put_Line("Du matade in " & S_2);
+      Put("Mata in en sträng på exakt 10 tecken: ");
+      Get(S_3);
+      Put("Du matade in " & S_3);
+      New_Line(2);
+      Skip_Line;
+   end Del2;
+
+   --Funktioner för del3
    function Leap_Year (Year : in Integer) return Boolean is
-      
    begin
       if ((Year mod 4 = 0 and (not (Year mod 100 = 0))) or Year mod 400 = 0) then
          return True;
@@ -94,34 +122,20 @@ procedure Test_Exceptions is
    end Get;
 
    --DEL 3
-   procedure Get (Item : out Date_Type) is
-      S : String(1..10);
-      begin
-         Get(S);
-         for I in S'Range loop
-            if S(5) /= '-' or S(8) /= '-' then
+   procedure Format_Error_Subprogram (S : in String) is
+   begin
+      for I in S'Range loop
+         if I in 1..4 or I in 6..7 or I in 9..10 then
+            if S(I) not in '0' .. '9' then
                raise Format_Error;
             end if;
-            for I in 1..4 loop
-               if S(I) not in '0'..'9' then
-                  raise Format_Error;
-               end if;
-            end loop;
-            for I in 6..7 loop
-               if S(I) not in '0'..'9' then
-                  raise Format_Error;
-               end if;
-            end loop;
-            for I in 9..10 loop
-               if S(I) not in '0'..'9' then
-                  raise Format_Error;
-               end if;
-            end loop;
-         end loop;
-         Item.Year  := Integer'Value(S(1..4));
-         Item.Month := Integer'Value(S(6..7));
-         Item.Day   := Integer'Value(S(9..10));
-         if Item.Year not in 1532..9000 then
+         end if;
+      end loop;
+   end Format_Error_Subprogram;
+
+   procedure YMD (Item : in Date_Type) is
+   begin
+      if Item.Year not in 1532..9000 then
             raise Year_Error;
          end if;
          if Item.Month not in 1..12 then
@@ -130,11 +144,22 @@ procedure Test_Exceptions is
          if Item.Day not in 1..Return_Days(Item) then
             raise Day_Error;
          end if;
-         Skip_Line;
+   end YMD;
+
+   procedure Get (Item : out Date_Type) is
+      S : String(1..10);
+      begin
+         Get(S);
+         Format_Error_Subprogram(S);
+         Item.Year  := Integer'Value(S(1..4));
+         Item.Month := Integer'Value(S(6..7));
+         Item.Day   := Integer'Value(S(9..10));
+         YMD(Item);
       end Get;
 
    procedure Put (Item : in Date_Type) is      
-      type Date_Elements is array (1..3) of Integer;
+      type Date_Elements is 
+         array (1..3) of Integer;
       Date_E : Date_Elements;
    begin
       Date_E := (Item.Year,Item.Month,Item.Day);
@@ -149,6 +174,46 @@ procedure Test_Exceptions is
       end loop;
    end Put;
 
+   procedure Get (Dates : out Date_Arr_Type) is --GET för del3
+   begin
+      Put_Line("Del 3:");
+      for I in Dates'range loop
+         loop
+            begin
+               Put("Mata in datum ");
+               Put(I, Width => 0);
+               Put(": ");
+               Get(Dates(I));
+               exit;
+            exception
+               when Format_Error =>
+                  Put("Felaktigt format! ");
+                  Skip_Line;
+               when Year_Error =>
+                  Put("Felaktigt år! ");
+                  Skip_Line;
+               when Month_Error =>
+                  Put("Felaktig månad! ");
+                  Skip_Line;
+               when Day_Error =>
+                  Put("Felaktig dag! ");
+                  Skip_Line;
+            end;
+         end loop;
+      end loop;
+   end Get;
+
+   procedure Put (Dates : in Date_Arr_Type) is --Put för del3
+   begin
+      for I in Dates'range loop
+         Put("Datum nummer ");
+         Put(I, Width => 0);
+         Put(": ");
+         Put(Dates(I));
+         New_Line;
+      end loop;
+   end Put;
+
    Min, Max, Value : Integer;
    S_1 : String(1 .. 5);
    S_2 : String(2 .. 7);
@@ -156,49 +221,11 @@ procedure Test_Exceptions is
    Dates : Date_Arr_Type;
 
 begin
-   --Put_Line("Del 1:");
-   --Put("Mata in Min och Max: ");
-   --Get(Min);
-   --Get(Max);
-   --Get_Value_Safe(Min, Max, Value);
-   --Put("Du matade in heltalet ");
-   --Put(Value, Width => 0);
-   --Put('.');
-   --New_Line(2);
-   --Skip_Line;
-   
-   --Put_Line("Del 2:");
-   --Put("Mata in en sträng på exakt 5 tecken: ");
-   --Get(S_1);
-   --Put_Line("Du matade in " & S_1);
-   --Put("Mata in en sträng på exakt 6 tecken: ");
-   --Get(S_2);
-   --Put_Line("Du matade in " & S_2);
-   --Put("Mata in en sträng på exakt 10 tecken: ");
-   --Get(S_3);
-   --Put("Du matade in " & S_3);
-   --New_Line(2);
-   --Skip_Line;
-
-   Put_Line("Del 3:");
-   for I in Dates'range loop
-      Put("Mata in datum ");
-      Put(I, Width => 0);
-      Put(": ");
-      Get(Dates(I));
-   end loop;
-   
-   for I in Dates'range loop
-      Put("Datum nummer ");
-      Put(I, Width => 0);
-      Put(": ");
-      Put(Dates(I));
-      New_Line;
-   end loop;
---exception
---   when Length_Error =>
---      Put("För få inmatade tecken:");
---      --Skip_Line;
-
-
+   Del1(Min,Max,Value);
+   Del2(S_1,S_2,S_3);
+   Get(Dates);
+   Put(Dates);
+   exception
+      when Length_Error =>
+      Put("För få inmatade tecken!");
 end Test_Exceptions;
--- duggap2v4/duggap2v4/src/duggap2v4.adb
+++ duggap2v4/duggap2v4/src/duggap2v4.adb
@@ -2,16 +2,18 @@ with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Float_Text_IO; use Ada.Float_Text_IO;
 procedure Duggap2v4 is
-   --Del1
-   function Del1 (S : in String) return Integer is
-   begin
-      return Integer(Float'Value(S); --NOTE - Gör först om till en float då vi i körexempel 3 matar in strängen "0.0", annars får vi exception error
-   end Del1;
+--Del1
+function Del1 (S : in String) return Integer is
+SF : Float;
+begin
+   SF := Float'Value(S);            --NOTE - Gör först om till en float då vi i körexempel 3 matar in strängen "0.0", annars får vi exception error
+   return Integer(SF);
+end Del1;
 
-   --Del2
-   procedure Del2 (I : out Integer;
-		   F : out Float;
-		   K : out Float) is
+--Del2
+procedure Del2 (I : out Integer;
+		         F : out Float;
+		         K : out Float) is
    begin
       Put("Mata in ett heltal: ");
       Get(I);
@@ -20,21 +22,21 @@ procedure Duggap2v4 is
       Put("Du matade in heltalet: ");
       Put(I, Width => 0);
       Put(" och flyttalet: ");
-      Put(F, Fore => 0, Aft => 0, Exp => 0);
+      Put(F,0,0,0);
       Put(" och kvoten blev ");
       K := Float(Float'Floor(Float(I) / F));
    end Del2;
 
-   --Del3
-   function "*" (Left : in String;
-		 Right : in Float) return Integer is
-   begin
-      return Integer(Float(Float'Rounding(Float'Value(Left) * Right)));
-   end "*";
+--Del3
+function "*" (Left : in String;
+	Right : in Float) return Integer is
+begin
+   return Integer(Float(Float'Rounding(Float'Value(Left) * Right)));
+end "*";
 
-   S : String(1..3);
-   I : Integer;
-   F, K : Float;
+S : String(1..3);
+I : Integer;
+F, K : Float;
 
 begin
    --Del 1
@@ -58,7 +60,7 @@ begin
    Get(F);
    Put("Den beräknade produkten av " & S);
    Put(" och ");
-   Put(F, Fore => 0, Aft => 0, Exp => 0);
+   Put(F, 0, 0, 0);
    Put(" blir ");
    Put(S * F, Width => 0);
 end Duggap2v4;
