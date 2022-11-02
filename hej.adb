f8ac509185fae0cd4e813e5a67b537a5a89255a0
Palmis <melvin_02@hotmail.se>
2022-11-02 10:35:57 +0100

    o4_done

--- hej.adb
+++ hej.adb
@@ -1,5 +1,29 @@
-procedure hej () is
+procedure Main () is
     
+    type Int_Pointer is 
+        access Integer;
+
+    P1,P2 : Int_Pointer;
+    I1, I2 : Integer;
+
+begin
+    P1 := null;
+
+    P2 := new Integer;
+
+    P1 := P2;
+
+
+end Main;
+
+
+
+procedure Main () is
+    
+    type Person_Ptr is  --pekare till en person
+        access Person_Type  --person_type innehåller skonummer, namn osv på en person
+
+
 begin
     
-end hej;
+end Main;
